2d63f3ca3706c8ff57f4676b2e3eb949cc1d1c12
Chris West (Faux) <git@goeswhere.com>
2017-07-05 09:33:58 +0100

    improve readability

--- pycronym.py
+++ pycronym.py
@@ -1,51 +1,8 @@
-#!/usr/bin/python
-import string
+#!/usr/bin/env python3
 import random
 import sys
+import itertools
 
-reltime = random #aHOHOO!
-
-def readfilewords(filename):
-    d = {}
-    for c in string.ascii_uppercase:
-        d[c] = []
-    f = open(filename)
-    for line in f:
-        c = string.upper(line[0])
-        d[c].append(line.strip())
-    return d
-    
-nouns = readfilewords('nouns.txt')
-propernouns = readfilewords('propernouns.txt')
-verbs = readfilewords('verbs.txt')
-adjectives = {}
-
-def pycronym(acronym):
-    """ Acronym expander.
-    
-    Arguments:
-    cronym: A string acronym.
-
-    Example:
-    pycronym("LOL") => "Lisa Outrageously Laughs"
-    """
-    line = []
-    i = 0
-    for c in acronym:
-        if i % 3 == 0:
-            line.append(reltime.choice(propernouns[c]))
-        elif i % 3 == 1:
-            line.append(reltime.choice(verbs[c]))
-        else:
-            line.append(reltime.choice(nouns[c]))
-        i += 1
-    return line
-
-
-def main():
-    for arg in sys.argv[1:]:
-        print arg.upper() + ' => ' + ' '.join(pycronym(arg.upper()))
-
-main()
-
+w=[{k:list(v) for k,v in itertools.groupby(open(f),lambda l:l[0])} for f in['nouns.txt','propernouns.txt','verbs.txt']]
+print('\n'.join(a.upper()+' => '+' '.join((random.choice(w[i%3][c]).strip() for i,c in enumerate(a.upper()))) for a in sys.argv[1:]))
 
